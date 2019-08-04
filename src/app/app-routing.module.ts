b4ad27755ff279a63b3fa1b2222fc50ac440e5fa
theolympicdream <theolympicdream2020@gmail.com>
2019-08-04 23:01:03 +0530

    added sidenav to home comp.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 // components
 import { HomeComponent } from './home/home.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
