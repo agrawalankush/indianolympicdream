4cb988f8598dbd7f91e10a3595cc1cb85649a652
theolympicdream <theolympicdream2020@gmail.com>
2019-08-11 00:58:24 +0530

    removed componets from sports module

--- src/app/sports/sport-routing.module.ts
+++ src/app/sports/sport-routing.module.ts
@@ -2,23 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 //Components
-import { SportQualifyingSysystemComponent } from './sport-qualifying-sysystem/sport-qualifying-sysystem.component';
-import { SportQualifyingCalenderComponent } from './sport-qualifying-calender/sport-qualifying-calender.component';
-import { QualifiedAthletesComponent } from './qualified-athletes/qualified-athletes.component';
-import { PotentialtAthletesComponent } from './potentialt-athletes/potentialt-athletes.component';
+
 import { SportHistoryComponent } from './sport-history/sport-history.component';
 import { SportHomeComponent } from './sport-home/sport-home.component';
 
 const routes: Routes = [
 
   { path: 'sports/:sportname', 
-    component: SportHomeComponent,
-    children:[
-      { path: 'qualifying',  component: SportQualifyingSysystemComponent },
-      { path: 'calendar',  component: SportQualifyingCalenderComponent },
-      { path: 'athletes',  component: QualifiedAthletesComponent },
-      { path: 'history',  component: SportHistoryComponent }
-    ]
+    component: SportHomeComponent
+    // children:[
+    //   { path: 'history',  component: SportHistoryComponent }
+    // ]
    }  
 ];
 
--- src/app/sports/sport.module.ts
+++ src/app/sports/sport.module.ts
@@ -4,21 +4,14 @@ import { MaterialModule } from '../material/material.module';
 import { SportRoutingModule } from './sport-routing.module';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
 //Components
-import { SportQualifyingSysystemComponent } from './sport-qualifying-sysystem/sport-qualifying-sysystem.component';
-import { SportQualifyingCalenderComponent } from './sport-qualifying-calender/sport-qualifying-calender.component';
-import { QualifiedAthletesComponent } from './qualified-athletes/qualified-athletes.component';
-import { PotentialtAthletesComponent } from './potentialt-athletes/potentialt-athletes.component';
-import { SportHistoryComponent } from './sport-history/sport-history.component';
+
+ import { SportHistoryComponent } from './sport-history/sport-history.component';
 import { SportHomeComponent } from './sport-home/sport-home.component';
 
 
 
 @NgModule({
   declarations: [
-    SportQualifyingSysystemComponent, 
-    SportQualifyingCalenderComponent, 
-    QualifiedAthletesComponent, 
-    PotentialtAthletesComponent, 
     SportHistoryComponent, 
     SportHomeComponent
   ],
