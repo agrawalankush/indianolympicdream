10f6341fac566e6f052493f8d3b86e0e660b3c47
theolympicdream <theolympicdream2020@gmail.com>
2019-09-07 21:41:17 +0530

    added autocomplete for search

--- src/app/athletes/athletes.component.ts
+++ src/app/athletes/athletes.component.ts
@@ -1,10 +1,16 @@
-import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SportsdataService } from '../sportsdata.service';
-import { MatPaginator } from '@angular/material';
 import {PageEvent} from '@angular/material/paginator';
 // import { Athletes} from '../models/app-models';
 import { tap } from 'rxjs/operators';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {FormControl} from '@angular/forms';
+import {MatAutocompleteSelectedEvent, MatAutocomplete} from '@angular/material/autocomplete';
+import {MatChipInputEvent} from '@angular/material/chips';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
 @Component({
   selector: 'app-athletes',
   templateUrl: './athletes.component.html',
@@ -19,11 +25,27 @@ export class AthletesComponent implements OnInit{
   length: number;
   pageSize = 8;
   pageSizeOptions: number[] = [4, 8, 16,32];
-
   // MatPaginator Output
   pageEvent: PageEvent;
-  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
-  constructor(private route:ActivatedRoute,private sportservice:SportsdataService) { }
+  
+  // Search autocomplete Inputs
+  visible = true;
+  selectable = true;
+  removable = true;
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  sportCtrl = new FormControl();
+  filteredSports: Observable<any>;
+  sports: string[] = [];
+  allsports: string[] = ['Archery', 'Athletics', 'Boxing', 'Equestrian', 'Fencing','Golf', 'Gymnastics', 'Hockey',
+                         'Judo','Rowing','Shooting', 'TableTennis','Tennis', 'Weightlifting', 'Wrestling'];
+
+  @ViewChild('sportInput', {static: false}) sportInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto', {static: false}) matAutocomplete: MatAutocomplete;
+  constructor(private route:ActivatedRoute,private sportservice:SportsdataService) { 
+    this.filteredSports = this.sportCtrl.valueChanges.pipe(
+      startWith(null),
+      map((sport: string | null) => sport ? this._filter(sport) : this.getallsports()));
+  }
 
   ngOnInit() {
     this.route.data
@@ -52,4 +74,35 @@ export class AthletesComponent implements OnInit{
        console.log(error);
      });
    }
+   getallsports() { 
+   if(this.sports){ 
+   // console.log(this.allsports.filter(sport => !(this.sports.includes(sport))))  
+   return this.allsports.filter(sport => !(this.sports.includes(sport)));
+  } else {
+   return this.allsports.slice();
+  }
+   }
+   remove(sport: string): void {
+    const index = this.sports.indexOf(sport);
+
+    if (index >= 0) {
+      this.sports.splice(index, 1);
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.sports.push(event.option.viewValue);
+    this.sportInput.nativeElement.value = '';
+    this.sportCtrl.setValue(null);
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    if(this.sports.length > 0) {
+      let filteredallsports = this.allsports.filter(sport => !(this.sports.includes(sport)));
+      return filteredallsports.filter(sport => sport.toLowerCase().indexOf(filterValue) === 0);
+    } else {
+    return this.allsports.filter(sport => sport.toLowerCase().indexOf(filterValue) === 0);
+  }
+  }
 }
