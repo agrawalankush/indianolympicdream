fc32186c41903a83ac97f187f69546d97eed8614
agrawalankush <96ankush@gmail.com>
2025-05-09 19:30:38 +0530

    Updated Theme menu to be a toggle button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, HostBinding } from '@angular/core';
 import { SwupdateService } from './swupdate.service';
-import { OverlayContainer} from '@angular/cdk/overlay';
+import { OverlayContainer } from '@angular/cdk/overlay';
 import {
   Router,
   // import as RouterEvent to avoid confusion with the DOM Event
@@ -14,13 +14,13 @@ import {
 import { slideInAnimation } from './animations';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    animations: [
-        slideInAnimation
-    ],
-    standalone: false
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    slideInAnimation
+  ],
+  standalone: false
 })
 export class AppComponent {
   // Sets initial value to true to show loading spinner on first load
@@ -28,9 +28,11 @@ export class AppComponent {
   public isLightTheme = false;
   @HostBinding('class') componentCssClass;
   selectedtheme: string;
+  currentTheme = 'default-theme';
+
   constructor(private router: Router, private swupdateservice: SwupdateService, public overlayContainer: OverlayContainer) {
     const theme = localStorage.getItem('selectedTheme');
-    this.onSetTheme(theme);
+    this.onSetTheme();
     this.swupdateservice.checkForUpdates();
     router.events.subscribe((event: RouterEvent) => {
       this.navigationInterceptor(event);
@@ -53,31 +55,32 @@ export class AppComponent {
       this.loading = false;
     }
   }
-    // For dark and light theme mode
-    // changeTheme(): void {
-    //      if (this.isLightTheme) {
-    //         this.isLightTheme = false;
-    //         this.overlayContainer.getContainerElement().classList.add('default-theme');
-    //         this.componentCssClass = 'default-theme';
-    //      } else {
-    //         this.isLightTheme = true;
-    //         this.overlayContainer.getContainerElement().classList.add('light-theme');
-    //         this.componentCssClass = 'light-theme';
-    //     }
-    //  }
-     onSetTheme(theme) {
-      localStorage.setItem('selectedTheme', theme);
-      if (this.componentCssClass) {
-        this.overlayContainer.getContainerElement().classList.remove(this.componentCssClass);
-        this.componentCssClass = null;
-        this.overlayContainer.getContainerElement().classList.add(theme);
-        this.componentCssClass = theme;
-      } else {
-        this.componentCssClass = theme;
-        this.overlayContainer.getContainerElement().classList.add(theme);
-      }
+  // For dark and light theme mode
+  // changeTheme(): void {
+  //      if (this.isLightTheme) {
+  //         this.isLightTheme = false;
+  //         this.overlayContainer.getContainerElement().classList.add('default-theme');
+  //         this.componentCssClass = 'default-theme';
+  //      } else {
+  //         this.isLightTheme = true;
+  //         this.overlayContainer.getContainerElement().classList.add('light-theme');
+  //         this.componentCssClass = 'light-theme';
+  //     }
+  //  }
+  onSetTheme() {
+    this.currentTheme = this.currentTheme === "default-theme" ? "dark-theme" : "default-theme";
+    localStorage.setItem('selectedTheme', this.currentTheme);
+    if (this.componentCssClass) {
+      this.overlayContainer.getContainerElement().classList.remove(this.componentCssClass);
+      this.componentCssClass = null;
+      this.overlayContainer.getContainerElement().classList.add(this.currentTheme);
+      this.componentCssClass = this.currentTheme;
+    } else {
+      this.componentCssClass = this.currentTheme;
+      this.overlayContainer.getContainerElement().classList.add(this.currentTheme);
     }
-    prepareRoute(outlet: RouterOutlet) {
-      return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
-    }
+  }
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
+  }
 }
