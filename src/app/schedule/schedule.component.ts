03ebd955b77fef8d581d1d63b24b38e2b4f50eaa
agrawalankush <96ankush@gmail.com>
2025-05-09 17:50:55 +0530

    Updated Schedule component and styles

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit, ElementRef, ViewChild, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { SportsdataService } from '../sportsdata.service';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { EventDetailsComponent } from '../event-details/event-details.component';
 @Component({
-    selector: 'app-schedule',
-    templateUrl: './schedule.component.html',
-    styleUrls: ['./schedule.component.scss'],
-    standalone: false
+  selector: 'app-schedule',
+  templateUrl: './schedule.component.html',
+  styleUrls: ['./schedule.component.scss'],
+  standalone: false
 })
 export class ScheduleComponent implements OnInit, OnDestroy {
   schedule: any;
@@ -54,38 +54,38 @@ export class ScheduleComponent implements OnInit, OnDestroy {
     '07Aug',
   ];
   constructor(
-    private route:ActivatedRoute,
+    private route: ActivatedRoute,
     private router: Router,
     public dialog: MatDialog,
-    private sportservice:SportsdataService) {
-    }
+    private sportservice: SportsdataService) {
+  }
 
   ngOnInit(): void {
 
     this.route.queryParams
       .subscribe(params => {
         // console.log(params);
-        if(Object.keys(params).length === 0 && params.constructor === Object){
+        if (Object.keys(params).length === 0 && params.constructor === Object) {
           this.getSchedule('');
         } else {
           // console.log(params, params.date, params.sport);
-        if(params.sport === undefined){
-          this.getByDate = true;
-          this.sport = '';
-          this.date = params.date;
-          this.getScheduleByDate(params.date);
-       } else if(params.date === undefined) {
-        this.getByDate = false;
-        this.date = '';
-        this.sport = params.sport;
-        this.getSchedule(params.sport);
-       }
-        // this.pageIndex = parseInt(params.pageIndex);
-        // this.pageSize = parseInt(params.pazeSize);
+          if (params.sport === undefined) {
+            this.getByDate = true;
+            this.sport = '';
+            this.date = params.date;
+            this.getScheduleByDate(params.date);
+          } else if (params.date === undefined) {
+            this.getByDate = false;
+            this.date = '';
+            this.sport = params.sport;
+            this.getSchedule(params.sport);
+          }
+          // this.pageIndex = parseInt(params.pageIndex);
+          // this.pageSize = parseInt(params.pazeSize);
 
         }
       }
-    );
+      );
   }
   originalOrder() { return 0; }
   selectedSport(sport: string) {
@@ -94,45 +94,45 @@ export class ScheduleComponent implements OnInit, OnDestroy {
       [],
       {
         relativeTo: this.route,
-        queryParams: { sport: sport},
+        queryParams: { sport: sport },
         queryParamsHandling: 'merge'
       });
   }
   getSchedule(selectedport: string) {
     this.sportservice.getschedule(selectedport)
-    .subscribe(
-     (res:any) => {
-       // console.log(res);
-       this.schedule = res.schedule;
-       this.length = res.total;
-   },
-     (error:any) =>{
-       // console.log(error);
-       this.errmsg = error.error;
-     });
+      .subscribe(
+        (res: any) => {
+          // console.log(res);
+          this.schedule = res.schedule;
+          this.length = res.total;
+        },
+        (error: any) => {
+          // console.log(error);
+          this.errmsg = error.error;
+        });
   }
   getScheduleByDate(date: string) {
     this.getByDate = true;
     this.sportservice.getschedulebydate(date)
-    .subscribe(
-     (res:any) => {
-       // console.log(res);
-       this.schedule = res.schedule;
-       this.length = res.total;
-   },
-     (error:any) =>{
-       // console.log(error);
-       this.errmsg = error.error;
-     });
+      .subscribe(
+        (res: any) => {
+          // console.log(res);
+          this.schedule = res.schedule;
+          this.length = res.total;
+        },
+        (error: any) => {
+          // console.log(error);
+          this.errmsg = error.error;
+        });
   }
-  showEvents(sport: string,events: any, Bydate?: string): void {
-    if(Bydate) {
+  showEvents(sport: string, events: any, Bydate?: string): void {
+    if (Bydate) {
       sport = Bydate;
     }
     const dialogRef = this.dialog.open(EventDetailsComponent, {
       width: '80%',
       height: '80%',
-      data: {sportname: sport, events: events},
+      data: { sportname: sport, events: events },
       panelClass: 'custom-dialog'
     });
     // dialogRef.afterClosed().subscribe(result => {
