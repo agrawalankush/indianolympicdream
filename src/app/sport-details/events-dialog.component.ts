18a8a6e61c191abcff388819337a6895a2bcc27f
agrawalankush <96ankush@gmail.com>
2025-05-10 23:20:27 +0530

    Updated Sports Detail Component styles and added Geist Fonts

---
+++ src/app/sport-details/events-dialog.component.ts
@@ -0,0 +1,78 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
+
+interface DialogData {
+    events: any[];
+    category: string;
+    sportName: string;
+}
+
+@Component({
+    selector: 'app-events-dialog',
+    template: `
+    <h2 mat-dialog-title>{{data.category | titlecase}} Events</h2>
+    <mat-dialog-content>
+      <ng-container [ngSwitch]="data.sportName">
+        <table *ngSwitchCase="'Athletics'" mat-table [dataSource]="data.events">
+          <ng-container matColumnDef="Event">
+            <th mat-header-cell *matHeaderCellDef>Event</th>
+            <td mat-cell *matCellDef="let element">{{element.name}}</td>
+          </ng-container>
+          <ng-container matColumnDef="EntryStandard">
+            <th mat-header-cell *matHeaderCellDef>Entry Standard</th>
+            <td mat-cell *matCellDef="let element">{{element.qualificationstandard}}</td>
+          </ng-container>
+          <ng-container matColumnDef="NR">
+            <th mat-header-cell *matHeaderCellDef>NR</th>
+            <td mat-cell *matCellDef="let element">{{element.NR}}</td>
+          </ng-container>
+          <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
+          <tr mat-row *matRowDef="let row; columns: displayedColumns"></tr>
+        </table>
+        
+        <mat-list *ngSwitchDefault>
+          <mat-list-item *ngFor="let event of data.events">
+            <span matListItemTitle>{{event.name}}</span>
+            <span matListItemLine *ngIf="event.category">{{event.category}}</span>
+            <div matListItemLine *ngIf="isJudoMixed(event)">
+              Men-{{event.men}} | Women-{{event.women}}
+            </div>
+          </mat-list-item>
+        </mat-list>
+      </ng-container>
+    </mat-dialog-content>
+    <mat-dialog-actions align="end">
+      <button mat-button mat-dialog-close>Close</button>
+    </mat-dialog-actions>
+  `,
+    styles: [`
+    :host {
+      display: block;
+    }
+    table { 
+      width: 100%;
+    }
+    .mat-mdc-dialog-content { 
+      max-height: 70vh;
+      padding: 16px;
+    }
+  `],
+    standalone: true,
+    imports: [CommonModule, MatDialogModule, MatTableModule, MatButtonModule, MatListModule]
+})
+export class EventsDialogComponent {
+    readonly displayedColumns = ['Event', 'EntryStandard', 'NR'];
+
+    constructor(
+        public dialogRef: MatDialogRef<EventsDialogComponent>,
+        @Inject(MAT_DIALOG_DATA) public data: DialogData
+    ) { }
+
+    private isJudoMixed(event: any): boolean {
+        return this.data.sportName === 'Judo' && this.data.category === 'mixed';
+    }
+}
--- src/app/sport-details/sport-details.component.ts
+++ src/app/sport-details/sport-details.component.ts
@@ -1,36 +1,70 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SportsdataService } from '../sportsdata.service';
-import { SportDetails } from '../models/app-models';
+import { MatDialog } from '@angular/material/dialog';
+import { EventsDialogComponent } from './events-dialog.component';
+import { map, switchMap } from 'rxjs/operators';
+
+interface SportEvents {
+  key: string;
+  value: any[];
+}
+
 @Component({
-    selector: 'app-sport-details',
-    templateUrl: './sport-details.component.html',
-    styleUrls: ['./sport-details.component.scss'],
-    standalone: false
+  selector: 'app-sport-details',
+  templateUrl: './sport-details.component.html',
+  styleUrls: ['./sport-details.component.scss'],
+  standalone: false
 })
 export class SportDetailsComponent implements OnInit {
-  public errmsg: string;
-  public sportname:string;
-  public sportsdetails:any;
+  sportsdetails: any;
   qualifiedAthletesString: string;
-  allsports: string[] = ['Archery', 'Athletics','Badminton', 'Boxing', 'Equestrian', 'Fencing','Golf', 'Gymnastics', 'Hockey',
-                         'Judo','Rowing','Shooting', 'Sailing', 'Swimming', 'TableTennis','Tennis', 'Weightlifting', 'Wrestling'];
-  public displayedColumns: string[] = ['Event', 'EntryStandard', 'NR'];
+  selectedCategory: string | null = null;
+  selectedEvents: any[] = [];
+
+  readonly allsports = ['Archery', 'Athletics', 'Badminton', 'Boxing', 'Equestrian',
+    'Fencing', 'Golf', 'Gymnastics', 'Hockey', 'Judo', 'Rowing', 'Shooting',
+    'Sailing', 'Swimming', 'TableTennis', 'Tennis', 'Weightlifting', 'Wrestling'];
+
+  private readonly categoryIcons: Record<string, string> = {
+    men: 'man',
+    women: 'woman',
+    mixed: 'groups',
+    unisex: 'wc'
+  };
+
   constructor(
     private route: ActivatedRoute,
-    private sportservice:SportsdataService
+    private sportservice: SportsdataService,
+    private dialog: MatDialog
   ) { }
 
-  ngOnInit() {
-    this.route.paramMap.subscribe((params:any) => {
-      // console.log(params.params.sportname);
-      this.sportservice.getsports(params.params.sportname)
-    .subscribe(
-      (res: any) => {
-      // console.log(res);
+  ngOnInit(): void {
+    this.route.paramMap.pipe(
+      map(params => params.get('sportname')),
+      switchMap(sportname => this.sportservice.getsports(sportname))
+    ).subscribe(res => {
       this.sportsdetails = res[0];
       this.qualifiedAthletesString = JSON.stringify([this.sportsdetails.name]);
     });
-    })
+  }
+
+  getCategoryShorthand(category: string): string {
+    return this.categoryIcons[category.toLowerCase()] || category.charAt(0).toUpperCase();
+  }
+
+  showEvents(category: SportEvents): void {
+    this.selectedCategory = category.key;
+    this.selectedEvents = category.value;
+
+    this.dialog.open(EventsDialogComponent, {
+      data: {
+        events: this.selectedEvents,
+        category: this.selectedCategory,
+        sportName: this.sportsdetails.name
+      },
+      width: '90%',
+      maxWidth: '800px'
+    });
   }
 }
