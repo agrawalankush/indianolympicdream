9c598cc949e475cde6d7ad6c0f17364a5e43dad6
agrawalankush <96ankush@gmail.com>
2025-05-14 01:39:19 +0530

    Updated Home component with dark themed pictograms.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,28 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AllSportsResolved } from '../models/app-models';
 import { SportsdataService } from '../sportsdata.service';
+
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss'],
-    standalone: false
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  standalone: false
 })
 export class HomeComponent implements OnInit {
-  public errmsg:string;
+  public errmsg: string;
   public allsports: any;
-  constructor(private route:ActivatedRoute, private sportservice: SportsdataService) { }
+
+  constructor(
+    private route: ActivatedRoute,
+    private sportservice: SportsdataService
+  ) { }
 
   ngOnInit() {
     this.sportservice.getallsports()
-    .subscribe(
-      (res: any) => {
-      this.allsports = res;
-    });
+      .subscribe(
+        (res: any) => {
+          this.allsports = res;
+        });
   }
 }
