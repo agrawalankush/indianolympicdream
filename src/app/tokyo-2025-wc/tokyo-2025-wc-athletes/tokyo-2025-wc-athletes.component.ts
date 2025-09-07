e055ecaf91bef31aa32e03070111ac384d652071
Ankush <96ankush@gmail.com>
2025-09-07 11:05:35 +0530

    Added Athletes profile links

--- src/app/tokyo-2025-wc/tokyo-2025-wc-athletes/tokyo-2025-wc-athletes.component.ts
+++ src/app/tokyo-2025-wc/tokyo-2025-wc-athletes/tokyo-2025-wc-athletes.component.ts
@@ -8,6 +8,7 @@ import { of as observableOf } from 'rxjs';
 import { Tokyo2025WcDataService } from '../tokyo-2025-wc-data.service';
 
 interface Athlete {
+  id: number;
   name: string;
   sportname: string;
   event: string;
@@ -17,6 +18,7 @@ interface Athlete {
   date_qualified: number;
   PB?: string;
   SB?: string;
+  profileUrl?: string; // Added profileUrl
 }
 
 interface AthleteResponse {
@@ -40,6 +42,27 @@ export class Tokyo2025WcAthletesComponent implements OnInit {
   public errmsg: string;
   public athletes: Athlete[];
   public total: number;
+  profileLinks = {
+    1: 'https://worldathletics.org/athletes/india/neeraj-chopra-14549089',
+    2: 'https://worldathletics.org/athletes/india/sreeshankar-14734673',
+    3: 'https://worldathletics.org/athletes/india/annu-rani-14422692',
+    4: 'https://worldathletics.org/athletes/india/sandeep-kumar-14381862',
+    5: 'https://worldathletics.org/athletes/india/priyanka-14477764',
+    6: 'https://worldathletics.org/athletes/india/animesh-kujur-14971002',
+    7: 'https://worldathletics.org/athletes/india/gulveer-singh-15023839',
+    8: 'https://worldathletics.org/athletes/india/parul-chaudhary-14497992',
+    9: 'https://worldathletics.org/athletes/india/sachin-yadav-14925646',
+    10: 'https://worldathletics.org/athletes/india/yash-vir-singh-14818383',
+    11: 'https://worldathletics.org/athletes/india/rohit-yadav-14740932',
+    12: 'https://worldathletics.org/athletes/india/abdulla-aboobacker-narangolintevida-14606734',
+    13: 'https://worldathletics.org/athletes/india/sarvesh-anil-kushare-14675319',
+    14: 'https://worldathletics.org/athletes/india/servin-sebastian-14841043',
+    15: 'https://worldathletics.org/athletes/india/ram-baboo-14919137',
+    16: 'https://worldathletics.org/athletes/india/tejas-shirse-14873420',
+    17: 'https://worldathletics.org/athletes/india/pooja-14818407',
+    18: 'https://worldathletics.org/athletes/india/praveen-chithravel-14841034',
+    19: 'https://worldathletics.org/athletes/india/ankita-14818406'
+   };
 
   constructor(private tokyo2025WcDataService: Tokyo2025WcDataService) { } // Inject new service
 
@@ -56,8 +79,17 @@ export class Tokyo2025WcAthletesComponent implements OnInit {
         })
       )
       .subscribe((res: AthleteResponse) => {
-        this.athletes = res.athletes;
+        this.athletes = res.athletes.map(athlete => {
+          const profileUrl = this.profileLinks[athlete.id as keyof typeof this.profileLinks];
+          return { ...athlete, profileUrl };
+        });
         this.total = res.total;
       });
+  }
+
+  navigateToProfile(url: string | undefined): void {
+    if (url) {
+      window.open(url, '_blank', 'noopener noreferrer');
+    }
   }
 }
